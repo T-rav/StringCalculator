148e44e73df55f423f399ae871c6f663f2eaf35e
T-rav <tmfrisinger@gmail.com>
2017-09-04 15:06:46 +0200

    Init commit

---
+++ String.Calculator.Tests/StringCalculatorTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace StringCalculatorKata
+{
+    [TestFixture]
+    public class StringCalculatorTests
+    {
+        [Test]
+        public void Method_Whenx_Shouldy()
+        {
+            //---------------Arrange-------------------
+
+            //---------------Act----------------------
+
+            //---------------Assert-----------------------
+            Assert.Fail("Test Not Yet Implemented");
+        }
+    }
+}
